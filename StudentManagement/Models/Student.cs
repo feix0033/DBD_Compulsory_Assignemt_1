bf078482be23cca841dbd2f00edacad7d0703e2a
feix0033 <evensnachi1@gmail.com>
2025-03-08 16:37:43 +0100

    Add DateOfBirth property to Student model and create corresponding database schema

--- StudentManagement/Models/Student.cs
+++ StudentManagement/Models/Student.cs
@@ -4,8 +4,9 @@ public class Student
 {
 	public int Id { get; set; }
 	public string? FirstName { get; set; }
-	public string? LastName { get; set; }
 	public string? MiddleName { get; set; }
+	public string? LastName { get; set; }
+	public DateTime? DateOfBirth { get; set; }
 	public string? Email { get; set; }
 	public DateTime? EnrollmentDate { get; set; }
 	public Enrollment? Enrollments { get; set; }
